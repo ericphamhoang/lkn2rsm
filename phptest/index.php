f5d2c4227837dff801bd2f6ed3d7d0789d776b85
Phương Phạm Hoàng <ericphamhoang@gmail.com>
2017-01-19 16:09:51 +1100

    Finish download button

--- phptest/index.php
+++ phptest/index.php
@@ -13,17 +13,26 @@ require_once __DIR__ . '/../config.php';
 global $twig;
 
 $html = $twig->render('default.twig', array(
-    'style' => file_get_contents(__DIR__.'/../assets/css/app.css')
+    'style' => file_get_contents(__DIR__ . '/../assets/css/app.css')
 ));
 
-$fileName = __DIR__.'/../output/wow';
+$fileName = __DIR__ . '/../output/wow';
 
-file_put_contents($fileName.".html", $html);
+file_put_contents($fileName . ".html", $html);
 
-$pdf = new \mikehaertl\wkhtmlto\Pdf($fileName.'.html');
+$pdf = new \mikehaertl\wkhtmlto\Pdf($fileName . '.html');
 
-$pdf->binary = __DIR__.'/../wkhtmltopdf/bin/wkhtmltopdf.exe';
+$pdf->binary = __DIR__ . '/../wkhtmltopdf/bin/wkhtmltopdf.exe';
 
-if (!$pdf->saveAs($fileName.'.pdf')) {
+if (!$pdf->saveAs($fileName . '.pdf')) {
     echo $pdf->getError();
 }
+
+?>
+    <body>
+    <link rel="stylesheet" href="../assets/css/app.css">
+    <section>
+        <a class="waves-effect waves-light btn" href="../output/wow.pdf" download="wow.pdf">Download</a>
+    </section>
+    </body>
+<?php
